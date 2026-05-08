e84eb9efe39a1247d90b2ca070e665764f532ec1
dayanandagowda222 <dayanandgowda.ty@gmail.com>
2026-05-08 11:59:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Good morning spiders !!!, how are you? ping ping ping"
+    return "Hello, Good morning spiders !!!, how are you? ping ping ping pong"
 
 @app.route('/health')
 def health():
